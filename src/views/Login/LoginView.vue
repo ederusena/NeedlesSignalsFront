28b51500c4edf015ff4739eb6f281f731f8fb111
Eder Sena <eder.sena@live.com>
2023-11-22 00:57:21 -0300

    Remove user variable from LoginView.vue

--- src/views/Login/LoginView.vue
+++ src/views/Login/LoginView.vue
@@ -10,7 +10,6 @@
         Needles Signals
       </h2>
     </div>
-    {{ user }}
     <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
       <div class="px-4 py-8 bg-white shadow sm:rounded-lg sm:px-10">
         <form class="space-y-6">
